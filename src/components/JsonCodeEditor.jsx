3636860eda3e297c612cc877d9d1d3c7f38de946
zianejaz <zianejaz@gmail.com>
2022-12-15 19:43:29 -0500

    added and implement react-json-view

--- src/components/JsonCodeEditor.jsx
+++ src/components/JsonCodeEditor.jsx
@@ -1,16 +1,24 @@
-import JSONInput from "react-json-ide";
+import { useContext } from "react";
+import ReactJson from "react-json-view";
 
-const JsonCodeEditor = ({jsonData}) => {
+import { SkillsContext } from "../contexts/skills.context";
 
+const JsonCodeEditor = ({ jsonData }) => {
+  const data = useContext(SkillsContext);
+  const handleJsonChange = (event) => {
+    console.log(event);
+  };
   return (
-    <div className="w-1/2 p-4">
-      <JSONInput
-        style={{ container: { borderRadius: "0.5rem" }}}
-        placeholder={jsonData}
-        locale={jsonData}
-        width="full"
-        height = "400px"
+    <div className="w-1/2 p-4 h-96">
+       <ReactJson
+        src={data}
+        theme="monokai"
+        onEdit={(edit) => handleJsonChange(edit)}
+        displayDataTypes = {false}
+        style={{fontSize : '1rem', borderRadius : "0.5rem", padding: "2rem"}}
+        collapseStringsAfterLength= {5}
       />
+      
     </div>
   );
 };
